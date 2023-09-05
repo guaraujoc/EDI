5e1b1dea12cdc06b38d7fe707a17b92fcf281080
Gustavo <guaraujoc@usp.br>
2023-09-05 19:58:40 +0000

    Adicionei a main

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "cube.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  float area, volume;
+
+  Cube *c1 = Cube_create(2);
+
+  area = Cube_area(c1);
+  volume = Cube_volume(c1);
+
+  printf("The cube has %.2f cm² of area and %.2f cm³ of volumes.\n", area,
+         volume);
+
+  Cube_free(c1);
+
+  return 0;
+}
