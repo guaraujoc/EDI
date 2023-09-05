d6c30c03af9554164481acd53e2d03a610fead8f
Gustavo <guaraujoc@usp.br>
2023-09-05 20:21:47 +0000

    Adicionado a header

---
+++ cube.h
@@ -0,0 +1,8 @@
+ typedef struct cube Cube;
+
+ Cube* Cube_create(float x); // Create the cube
+ void Cube_free(Cube* c);  // Free the memory allocated for the cube
+ int Cube_acess(Cube* c, float* x);  // Acess the cube
+ int Cube_assign(Cube*c, float x);  // Assign a side to a cube
+ float Cube_area(Cube* c);  // Calculates the area of a cube
+ float Cube_volume(Cube* c); // Calculates the volume a cube
